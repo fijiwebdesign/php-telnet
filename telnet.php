0e7bccd7c2e36a9cf21a1764d10e57aca49abd2d
Gabe Lalasava <gabe.lalasava@hp.com>
2014-11-13 16:26:13 +0400

    Allow telnet with any protocol supported by php like tcp, udp, ssl, tls etc.

--- telnet.php
+++ telnet.php
@@ -8,24 +8,43 @@ function telnet() {
     global $argv, $argc;
      
     if ($argc < 3) :
-        die('Usage : php telnet.php hostname port');
+        die('
+    Usage : 
+        php ' . $argv[0] . ' hostname port [protocol]
+                hostname    - Host domain or IP to connect to
+                port        - Port number to connect to
+                protocol    - Optional protocol to use. Options: ' . implode(', ', stream_get_transports()) . '. Default is tcp.
+
+    Example: 
+        Telnet to Gmail imap server
+                php ' . $argv[0] . ' imap.gmail.com 993 ssl
+
+            ');
     endif;
      
     $host = $argv[1];
     $port = (int) $argv[2];
+    $protocol = isset($argv[3]) ? $argv[3] : 'tcp';
     
-    if (!$s = fsockopen($host, $port, $errno)) {
-        die('Unable to connect : ' . $host . ':' . $port);
+    // connecto to host
+    if (!$s = @fsockopen($protocol . '://' . $host, $port, $errno, $errstr)) {
+        die('Unable to connect to ' . $protocol . '://' . $host . ':' . $port . ' with error: ' . $errstr);
     }
-    stream_set_blocking($s, 0);
+
+    // 
+    if ($info = stream_get_meta_data($s) && isset($info['timeout']) && $info['timeout']) {
+        die('Unable to connect: Connection timed out...');
+    }
      
-    echo 'Connected to remote host' . PHP_EOL;
+    echo 'Connected to host: ' . $protocol . '://' . $host . ':' . $port . PHP_EOL;
+
+    stream_set_blocking($s, 0);
      
     while (true) {
 
         $socket_list = array($s, STDIN);
          
-        # Get the list sockets which are readable
+        // Get the list sockets which are readable
         if (stream_select($socket_list , $write_sockets, $error_sockets, null) !== false) {
         
             foreach ($socket_list as $sock) {
@@ -33,7 +52,7 @@ function telnet() {
                 // incoming message from remote server
                 if ($sock == $s) {
                     if (!$data = stream_get_contents($s)) {
-                        die('Connection closed');
+                        die(PHP_EOL . 'Connection closed');
                      } else {
                         fputs(STDOUT, $data); // print data
                     }
